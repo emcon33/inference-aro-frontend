acc13884d6375bb0ca2bb28889969c91e815d387
Andy Grimes <48636570+emcon33@users.noreply.github.com>
2024-07-24 15:19:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -33,8 +33,8 @@ inputs = gr.inputs.Image(type='filepath')
 outputs = gr.outputs.Label(type="confidences",num_top_classes=5)
 
 # Define style
-title = "ROSA Workshop Gradio Front End"
-description = "This is the web front end to add a web interfface to an AI app on ROSA for image classification based on ResNet18. This is a port of an article linked below"
+title = "ARO Workshop Gradio Front End"
+description = "This is the web front end to add a web interfface to an AI app on ARO for image classification based on ResNet18. This is a port of an article linked below"
 article = "<p style='text-align: center'><a href='https://arxiv.org/abs/1512.03385' target='_blank'>ResNet18 Deep Residual Learning for Image Recognition</a> | <a href='https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py' target='_blank'>Github Repo</a></p>"
 
 # Run inference
